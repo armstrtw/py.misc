60920e08a3898b5f986f028db1540140e106114b
Whit Armstrong <armstrong.whit@gmail.com>
2022-01-18 08:48:28 -0500

    add comments

--- static_method.py
+++ static_method.py
@@ -6,6 +6,7 @@ class Cat:
     def meow():
         print("meow")
         ## needs self param to access members, which is not passed automatically when invoked
+        ## this will fail with: NameError: name 'self' is not defined
         ##print(self.name)
 
 
